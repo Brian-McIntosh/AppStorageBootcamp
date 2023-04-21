1b7a284ceb50016ab43c00f2d622db11df56c8a7
Brian McIntosh <brian@mixinteractive.com>
2023-04-21 12:16:20 -0400

    Initial Commit

---
+++ AppStorageBootcamp/AppStorageBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageBootcampApp.swift
+//  AppStorageBootcamp
+//
+//  Created by Brian McIntosh on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
